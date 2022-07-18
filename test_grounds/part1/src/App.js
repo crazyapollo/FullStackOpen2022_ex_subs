6a81ae8a95bc011a4685464d91d7ccada8485db2
crazyapollo <jwaizner@googlemail.com>
2022-07-18 16:19:15 +0200

    more testing

--- test_grounds/part1/src/App.js
+++ test_grounds/part1/src/App.js
@@ -1,42 +1,52 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const handleClick = () => {
-    console.log('clicked')
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
   }
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
 
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
   
-  return (
-    <div>
-        <Display counter={counter}/>
-        <button onClick={handleClick}>
-            plus log
-        </button>
-        <Button
-            onClick={increaseByOne}
-            text='plus'
-        />
-        <Button
-            onClick={setToZero}
-            text='zero'
-        />     
-        <Button
-            onClick={decreaseByOne}
-            text='minus'
-        />  
-    </div>
+    const handleLeftClick = () => {
+      setAll(allClicks.concat('L'))
+      setLeft(left + 1)
+    }
+  
+    const handleRightClick = () => {
+      setAll(allClicks.concat('R'))
+      setRight(right + 1)
+    }
     
-  )
-}
+    debugger 
+    
+    return (
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    )
+  }
 
 export default App
